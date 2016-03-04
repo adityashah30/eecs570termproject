b3fa93410458967639a7a81f42fe9651b9262351
Aditya Shah <adityashah30@gmail.com>
2016-03-03 21:16:41 -0500

    Updated populate empty as per Xiangfei's suggestion

--- code/preprocessing/populate_empty.sh
+++ code/preprocessing/populate_empty.sh
@@ -6,7 +6,7 @@ mv ../../data/links.csv.out ../../data/links.csv
 mv ../../data/movies.csv.out ../../data/movies.csv
 mv ../../data/ratings.csv.out ../../data/ratings.csv
 mv ../../data/tags.csv.out ../../data/tags.csv
-sed -i -e 's/^,/0,/' -e':a;s/,,/,0,/;ta' -e 's/,\s$/,0/' ../../data/links.csv
-sed -i -e 's/^,/0,/' -e':a;s/,,/,0,/;ta' -e 's/,\s$/,0/' ../../data/movies.csv
-sed -i -e 's/^,/0,/' -e':a;s/,,/,0,/;ta' -e 's/,\s$/,0/' ../../data/ratings.csv
-sed -i -e 's/^,/0,/' -e':a;s/,,/,0,/;ta' -e 's/,\s$/,0/' ../../data/tags.csv
+sed -i -e 's/^,/0,/' -e':a;s/,,/,0,/;ta' -e 's/,$/,0/' ../../data/links.csv
+sed -i -e 's/^,/0,/' -e':a;s/,,/,0,/;ta' -e 's/,$/,0/' ../../data/movies.csv
+sed -i -e 's/^,/0,/' -e':a;s/,,/,0,/;ta' -e 's/,$/,0/' ../../data/ratings.csv
+sed -i -e 's/^,/0,/' -e':a;s/,,/,0,/;ta' -e 's/,$/,0/' ../../data/tags.csv
