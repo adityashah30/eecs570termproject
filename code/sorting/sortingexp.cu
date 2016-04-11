be91a6bd9a4401368e5c3baa506353bc537054be
Aditya Shah <adityashah30@gmail.com>
2016-04-11 17:10:22 -0400

    Sorting exp added

---
+++ code/sorting/sortingexp.cu
@@ -0,0 +1,51 @@
+#include "sorting.cuh"
+#include "../timer/timer.h"
+#include <string>
+#include <cassert>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    Dataset originalInput;
+    Dataset input;
+    Dataset output;
+
+    string resultFile = "sortingScalingResults.txt";
+
+    int fieldIdx = 2;
+    int expCount = 1;
+
+    cout << "Loading data" << endl;
+    loadData(originalInput);
+    cout << "Data loaded" << endl;
+
+    cout << "Converting to nearest power of 2" << endl;
+    nearestPowerOf2DS(input, originalInput);
+    cout << "Conversion complete" << endl;
+
+    ofstream out(resultFile);
+    out << "#NumThreads Time" << endl;
+
+    for(int numThreads = 1; numThreads <= 1024; numThreads <<= 1)
+    {
+        Timer timer;
+        long long expTime = 0;
+        for(int c = 0; c < expCount; c++)
+        {
+            timer.startTimer();
+            sortData(output, input, fieldIdx, numThreads);
+            timer.stopTimer();
+            expTime += timer.getElapsedTime();
+        }
+        expTime /= expCount;
+        cout << "Time to sort data on " << numThreads << " threads : " 
+             << expTime << endl;
+        out << numThreads << " " << expTime << endl;
+    }
+
+    out.close();
+
+    return 0;
+}
