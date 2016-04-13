b440e1ea7be124d3a2a268e9183124aaa509ca7e
Chenxi Lou <ericlou@caen-vnc-oncampus02.engin.umich.edu>
2016-04-13 16:49:08 -0400

    groupexp.cu update

--- code/aggregation/groupexp.cu
+++ code/aggregation/groupexp.cu
@@ -1,4 +1,4 @@
-#include "group.h"
+#include "group.cuh"
 #include "../timer/timer.h"
 #include <string>
 #include <cassert>
@@ -6,75 +6,47 @@
 
 using namespace std;
 
-void populateData(Dataset& input, Dataset& expectedOutput);
+//void populateData(Dataset& input, Dataset& expectedOutput);
 
-static bool compare(const Record& first, const Record& second){
-	return first.movieId < second.movieId;
-}
+//static bool compare(const Record& first, const Record& second){
+//	return first.movieId < second.movieId;
+//}
 
 int main()
 {
+//    Dataset originalInput;
     Dataset input;
     Dataset output;
-	Dataset expectedOutput;
-	Timer timer;
 	
-    int numThreads = 1;
-    
-	for(; numThreads < 9; ++numThreads){
-		populateData(input, expectedOutput);
-		
-		timer.startTimer();
-		group(output, input, numThreads);
-		timer.stopTimer();
-		
-		sort(output.begin(), output.end(), compare);
-		
-		/*cout << "My output" << endl;
-		
-		for(auto it : output)
-			cout << it.movieId << ' ' << it.rating << endl;
-		
-		cout << "Expected output" << endl;
-		
-		for(auto it : expectedOutput)
-			cout << it.movieId << ' ' << it.rating << endl;*/
-		
-		assert(output.size() == expectedOutput.size());
-		
-		for(int i = 0; i < output.size(); ++i){
-			assert(output[i].movieId == expectedOutput[i].movieId);
-			assert(output[i].rating == expectedOutput[i].rating);
-		}
-		
-		//assert(output == expectedOutput);
-		
-		cout << "Time to group data on " << numThreads << " threads : " 
-              << timer.getElapsedTime() << std::endl;
-    }
+	int expCount = 20;
 	
-    return 0;
-}
+    string resultFile = "AggregationScalingGPUResults.txt";	
+	
+    cout << "Loading data" << endl;
+    loadData(input);
+    cout << "Data loaded" << endl;
 
-void populateData(Dataset& input, Dataset& expectedOutput){
-	input.clear();
-	expectedOutput.clear();
-	
-	int numRecords = 1024;
-	int offset = 123;
-	int numid = 128;
-	
-	for(int i = 0; i < numRecords/numid; ++i){
-		double total = 0;
-		for(int j = 0; j < numid; ++j){
-			double rating = rand()%5;
-			Record inputRecord = {0, i, rating, 0 };
-			total += rating;
-			input.push_back(inputRecord);
-		}
-		double avg = total/numid;
-		Record outputRecord = {0, i, avg, 0};
-		expectedOutput.push_back(outputRecord);
-	}
-	
-}
+    ofstream out(resultFile);
+    out << "#NumThreads Time" << endl;
+
+    for(int numThreads = 32; numThreads <= 1024; numThreads <<= 1)
+    {
+        Timer timer;
+        long long expTime = 0;
+        for(int c = 0; c < expCount; c++)
+        {
+            timer.startTimer();
+            gorup(output, input, numThreads);
+            timer.stopTimer();
+            expTime += timer.getElapsedTime();
+        }
+        expTime /= expCount;
+        cout << "Time to sort data on " << numThreads << " threads : " 
+             << expTime << endl;
+        out << numThreads << " " << expTime << endl;
+    }
+
+    out.close();
+
+    return 0;
+}	
